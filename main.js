92f339f4384c634cc14d9e1938bd743aeb6940a3
Łukasz <pawela89@gmail.com>
2020-08-10 16:32:58 +0200

    add main.js + footer changes

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 
-const boxes = [...document.querySelectorAll('.form-box__radio-input')]
+const radios = document.getElementsByName('check');
 
 
-document.addEventListener('click', function(){
+document.querySelector(".btn").addEventListener('click', function(){
+    for (i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
 
-})
+            if(i===0) {
+                console.log(`Picked answer ${i+1} - Buwegung`)
+            }else if(i===1){
+                console.log(`Picked answer ${i+1} - Gesunde Ernährung`)
+            }else if(i===2){
+                console.log(`Picked answer ${i+1} - Rauchen und Alkoholkonsum`)
+            }else if(i===3){
+                console.log(`Picked answer ${i+1} - Nein danke`)
+            }
+        }
+      }
+    })
+
+
+
+
+
+
 
